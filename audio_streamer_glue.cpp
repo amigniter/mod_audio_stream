61da172e5fbaafa7e333049a498bb9e3e62d7fbb
Soslan Aldatov <soslanaldatov@gmail.com>
2024-05-19 19:51:13 +0400

    add more logging

--- audio_streamer_glue.cpp
+++ audio_streamer_glue.cpp
@@ -678,6 +678,7 @@ extern "C"
                     switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "validate_address: portStart increased and now = %s\n", portStart);
                 }
                 *port = atoi(hostEnd + 1);
+                switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "validate_address: port is assigned\n");
                 switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "validate_address: port = %s\n", *port);
                 std::strncpy(tcpAddress, address, hostEnd - hostStart);
                 tcpAddress[hostEnd - hostStart] = '\0';
