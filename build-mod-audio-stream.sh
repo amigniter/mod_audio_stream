cbb215ad318137fad7fde0943318bc3d35c30b31
Evgenii Buchnev <40753176+evgeniibuchnev@users.noreply.github.com>
2024-07-09 20:51:56 +1000

    Exporting PKG_CONFIG_PATH only if path exists

--- build-mod-audio-stream.sh
+++ build-mod-audio-stream.sh
@@ -10,7 +10,11 @@ apt-get -y install libfreeswitch-dev libssl-dev zlib1g-dev libspeexdsp-dev
 
 git submodule init
 git submodule update
-export PKG_CONFIG_PATH=/usr/local/freeswitch/lib/pkgconfig
+
+FS_PKGCONFIG=/usr/local/freeswitch/lib/pkgconfig
+if [ -d "$FS_PKGCONFIG" ]; then
+    export PKG_CONFIG_PATH=$FS_PKGCONFIG
+fi
 
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
