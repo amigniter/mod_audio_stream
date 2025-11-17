e11a1ce9c7cfa9c3da397b1a18893a682c541ef2
NFhook <telegant@qq.com>
2025-11-17 11:27:13 +0800

    mod_audio_stream: Add ASR JSON as event header instead of event body

--- mod_audio_stream.c
+++ mod_audio_stream.c
@@ -15,7 +15,9 @@ static void responseHandler(switch_core_session_t* session, const char* eventNam
     switch_channel_t *channel = switch_core_session_get_channel(session);
     switch_event_create_subclass(&event, SWITCH_EVENT_CUSTOM, eventName);
     switch_channel_event_set_data(channel, event);
-    if (json) switch_event_add_body(event, "%s", json);
+    if (json) {
+        switch_event_add_header(event, SWITCH_STACK_BOTTOM, "AudioStream-ASR", json);
+    }
     switch_event_fire(&event);
 }
 
